7fd76c7dc5db6198a4689f47a75babad2d715c68
Diogo Freire <diogoffreire1@gmail.com>
2025-02-27 22:09:19 +0000

    Organizei os ficheiros da aulas e adicionei mais contéudo do curso

---
+++ CS50_Python/Aulas_curso/4_Exceptions/4.2_CS50_exceptions_ValueError.py
@@ -0,0 +1,7 @@
+x = int(input("What's x? "))
+print(f"x is {x}")
+
+# ValueError acontece quando o usuário digita algo que não 
+# condiz com o tipo de dados esperado e aqui o que nós esperamos é 
+# um número inteiro mas se o usuário digitar "gato", por exemplo,
+# o programa irá gerar um ValueError
