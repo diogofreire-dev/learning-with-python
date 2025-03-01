9d7828105b6139e1e878344eb93ba27c21e30f92
Diogo Freire <diogoffreire1@gmail.com>
2025-03-01 21:34:19 +0000

    Adicionei o exercicios do curso de CS50 sobre libraries.

---
+++ CS50_Python/Aulas_curso/5_Libraries.py/5.2_CS50_libraries_generate2.py
@@ -0,0 +1,4 @@
+import random 
+
+number = random.randint(1,10)
+print(number) # prints a random number between 1 and 10
---
+++ CS50_Python/Aulas_curso/5_Libraries.py/5.3_CS50_libraries_generate2.py
@@ -0,0 +1,7 @@
+import random
+
+cards = ["jack", "queen", "king"]
+random.shuffle(cards)
+
+for i in cards:
+    print(i)
